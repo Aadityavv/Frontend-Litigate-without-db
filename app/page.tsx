fe9e0aaf90fb0dcfe5b8ff2c989016ba8ce7f711
Aadityavv <aadityavv9@gmail.com>
2025-07-08 15:58:46 +0530

    frontend updatedd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 'use client';
+import React, { Suspense } from 'react';
 import UnifiedDashboardComponent from "@/components/unified-dashboard"
+
 export default function Page() {
-  
-  return <UnifiedDashboardComponent />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UnifiedDashboardComponent />
+    </Suspense>
+  );
 }
